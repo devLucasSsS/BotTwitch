9aa4f7e21c7016aba74efdb77705d9ff237b61ac
Eduardo Maturana <edu.maturana.c@gmail.com>
2022-10-14 21:55:04 -0300

    change to nodejs

---
+++ index.js
@@ -0,0 +1,31 @@
+const tmi = require("tmi.js");
+
+const client = new tmi.Client({
+  options: { debug: true },
+  identity: {
+    username: "lucassss14",
+    password: "oauth:o8gapeamhl62m8h2p1mivx3bshn84k",
+  },
+  channels: ["Mathi"],
+});
+
+client.connect();
+
+client.on("message", (channel, tags, message, self) => {
+  if (self) return;
+
+  if (message.toLowerCase() === "!hello") {
+    client.say(channel, `@${tags.username}, heya!`);
+  }
+
+  if (message.toLowerCase() === "!afk") {
+    client.say(channel, `Mathi will be back in a few minutes.`);
+  }
+
+  if (message.toLowerCase() === "!subathon") {
+    client.say(
+      channel,
+      `1 Sub = +1 Minute (Max 12 hours)\n\nSub milestones and more info: https://twitter.com/Mathi501/status/1579241459038842881?t=gWlRLEPtvKPmpNzBaOmvqw&s=19`
+    );
+  }
+});
